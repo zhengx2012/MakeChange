fe02b0d78d06cc7c65a94521b074a7754143ca3f
Xian Zheng <xzheng2112@gmail.com>
2018-03-03 11:33:42 -0700

    User story 1&2 created

--- src/com/skilldistillery/homework/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/homework/makechange/MakeChangeApp.java
@@ -1,10 +1,20 @@
 package com.skilldistillery.homework.makechange;
 
+import java.util.Scanner;
+
 public class MakeChangeApp {
+	static Scanner kb;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		kb = new Scanner(System.in);
+		
+		System.out.print("What is the price of the product? $");
+		double price = kb.nextDouble();
+		
+		System.out.print("\nHow much will you use to pay? $");
+		double priceTendered = kb.nextDouble();
+
+		kb.close();
 
 	}
-
 }
