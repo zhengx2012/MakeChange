1dc681c143874837676697de802b0cdf6fd52507
Xian Zheng <xzheng2112@gmail.com>
2018-03-03 21:50:06 -0700

    cash register will return dollars, but not a precise amount using a do while loop.  Updated readme.md.

--- README.md
+++ README.md
@@ -13,6 +13,12 @@ Users will be prompted to input the price of an item, then input the amount they
 
 
 #### How To Run This Project
+* Input a value when prompted for price of item. 
+* Input the amount you would like to use pay for it in the next prompt.  
+* If amount is equal to the price, computer will output a line.
+* If amount is less than the price, you will be prompted to add additional value.
+* System will print the change in the amount of bills and coins.
 
 
 #### Lessons Learned
+* 
--- src/com/skilldistillery/homework/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/homework/makechange/MakeChangeApp.java
@@ -7,52 +7,83 @@ public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		kb = new Scanner(System.in);
-		
+
 		System.out.print("What is the price of the product? $");
 		double price = kb.nextDouble();
-		
+
 		System.out.print("How much will you use to pay? $");
 		double priceTendered = kb.nextDouble();
 		
-		if (price == priceTendered) {
-			System.out.println("Thank for using the register, you paid the exact amount.");
-		}
-		else if (price > priceTendered) {
-			System.out.println("That amount is not enough, please insert more money. $");
-			double additionalTendered = kb.nextDouble();
-			priceTendered += additionalTendered;
-			
-		} else {
-			double remainder = priceTendered - price;
-			int billRemainder =  (int) remainder;
-			billCalc(remainder);
-			System.out.println();
-			
-			double changeRemainder = remainder; 
-			changeRemainder %= changeRemainder;
-			changeCalc(changeRemainder);
-		}
-		
+		System.out.println("Your change will be:");
+		cashRegister(price, priceTendered);
+
 		kb.close();
 
 	}
-	public static void billCalc(double bill) {
-		String changeBills [] = {"20 dollar bills", "10 dollar bills", "5 dollar bills", "1 dollar bills"};
-		for (int i = 0; i < changeBills.length; i++) {
-			System.out.println(changeBills[i]);
-//			if (changeBills[i]) {
-//				
-//			}
-//			
+	public static void cashRegister(double price, double priceTendered) {
+		if (price == priceTendered) {
+			System.out.println("Thank for using the register, you paid the exact amount.");
+		} else if (price > priceTendered) {
+			System.out.println("That amount is not enough, please insert more money. $");
+			double additionalTendered = kb.nextDouble();
+			priceTendered += additionalTendered;
+
+		} else {
+			double remainder = priceTendered - price;
+			int billRemainder = (int) remainder;
+			billCalc(billRemainder);
+			System.out.println();
+
+			double changeRemainder = remainder;
+			changeRemainder %= changeRemainder;
+			changeCalc(changeRemainder);
 		}
-		
-		
 	}
-	public static void changeCalc(double change) {
-		String changeCoins [] = {"Quarters", "Dimes", "Nickels", "Pennys"};
-		for (int i = 0; i < changeCoins.length; i++) {
-			System.out.println(changeCoins[i]);
+
+	/**
+	 * 
+	 * @param bill
+	 */
+	public static void billCalc(int billRemainder) {
+		String changeBills[] = { " twenty dollar bills", " ten dollar bills", " five dollar bills", " one dollar bills" };
+		int amount[] = { 20, 10, 5, 1 };
+		int extraBills = 0;
+		for (int i = 0; i < amount.length; i++) {
+			do {
+				if (billRemainder > amount[0]) {
+					extraBills %= billRemainder / amount[0];
+					billRemainder = billRemainder /amount[0];
+					System.out.println(billRemainder + changeBills[0]);
+				} 
+				if(billRemainder > amount[1]) {
+					billRemainder = billRemainder / amount [1];
+					System.out.println(billRemainder + changeBills[1]);
+				}
+				if(billRemainder > amount[2]) {
+					billRemainder = billRemainder / amount [2];
+					System.out.println(billRemainder + changeBills[2]);
+				}
+				if(billRemainder > amount[3]) {
+					billRemainder = billRemainder / amount [3];
+					System.out.println(billRemainder + changeBills[3]);
+				}
+
+			} while (billRemainder > 0);
 		}
-		
+
+	}
+
+	public static void changeCalc(double changeRemainder) {
+		String changeCoins[] = { "quarters", "dimes", "nickels", "pennies" };
+		for (int i = 0; i < changeCoins.length; i++) {
+			do {
+				if (changeRemainder > .25) {
+					changeRemainder = changeRemainder / .25;
+					System.out.println(changeRemainder + changeCoins[0]);
+				}
+				
+			}while(changeRemainder >= 0.01);
+		}
+
 	}
 }
